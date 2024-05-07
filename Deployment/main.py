a03395295d0eb10c83d76e99fe7b9a228f8d4368
Pravin Kalse <48201480+kalsep@users.noreply.github.com>
2024-05-07 11:44:25 +0530

    update filter

--- Deployment/main.py
+++ Deployment/main.py
@@ -2,24 +2,25 @@ from recommdator import *
 
 st.set_page_config(layout="wide")  # noqa: F405
 
-st.title("Bank Offers Recommdation System")
-st.divider()
-col1, col2 = st.columns([1, 3])
+st.title("Bank Offers Recommdation System")  # noqa: F405
+st.divider()  # noqa: F405
+col1, col2 = st.columns([1, 3])  # noqa: F405, F405
 search_text = col1.text_input(label="Input Text", placeholder="Brand, Category, Bank")
 recom_button = col1.button("Recommand Me Offer")
 
 
-if search_text != None and recom_button:
-    df = get_df()
-    filtered_index = filter_data(search_text)
-    if filtered_index != None:
+if search_text is not None and recom_button:
+    df = get_df()  # noqa: F405
+    filtered_index = filter_data(search_text)  # noqa: F405
+    if filtered_index is not None:
         offer_id = int(filtered_index[0])
 
         # st.subheader("Your Serach:")
         # st.table(df.iloc[filtered_index])
 
         col2.subheader("Your Recommdation:")
-        recommended_offers = generate_recommendations(offer_id)
+        col2.write("Top 10 Best Recommdation")
+        recommended_offers = generate_recommendations(offer_id)  # noqa: F405
         recommended_offers.rename(
             columns={
                 "Merchant Name": "Brand",
@@ -32,13 +33,13 @@ if search_text != None and recom_button:
         )
         recommended_offers.sample(frac=1)
 
-        with st.container():
+        with st.container():  # noqa: F405
             col2.dataframe(
                 recommended_offers,
                 use_container_width=True,
                 hide_index=True,
                 column_config={
-                    "Offer Link": st.column_config.LinkColumn("App URL"),
+                    "Offer Link": st.column_config.LinkColumn("App URL"),  # noqa: F405
                 },
             )
 
